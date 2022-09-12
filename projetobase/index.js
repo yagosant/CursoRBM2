d7a6dd5937b02e6ac8c1864c27aecc2663edd83b
Yago <yagojf27@gmail.com>
2022-09-12 20:21:11 -0300

    add função

--- projetobase/index.js
+++ projetobase/index.js
@@ -5,6 +5,9 @@ var Yago;
     Yago["nome"] = "Yago dos Santos";
     Yago["GitHub"] = "https://github.com/yagosant";
     Yago["Linkdln"] = "https://www.linkedin.com/in/yago-dos-santos-ribeiro-a45b6990/?ppe=1";
+    Yago["site"] = "https://yagosant.github.io";
 })(Yago || (Yago = {}));
+function getsite() { return Yago["site"]; }
 if (titleElement)
     titleElement.innerText = `Projeto Base do ${Yago["nome"]}`; // pegando os dados de um enum
+console.log(getsite());
--- projetobase/index.ts
+++ projetobase/index.ts
@@ -3,8 +3,13 @@ const titleElement = document.getElementById("title");
 enum Yago {
     "nome"= "Yago dos Santos",
     "GitHub" = "https://github.com/yagosant",
-    "Linkdln" = "https://www.linkedin.com/in/yago-dos-santos-ribeiro-a45b6990/?ppe=1"
+    "Linkdln" = "https://www.linkedin.com/in/yago-dos-santos-ribeiro-a45b6990/?ppe=1",
+    "site" = "https://yagosant.github.io"
     }
 
+function getsite() { return Yago["site"];}
+
 if (titleElement) titleElement.innerText = `Projeto Base do ${Yago["nome"]}`;// pegando os dados de um enum
+
+console.log(getsite());
 
