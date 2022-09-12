fd5d1bd0b94b3105102a0b61606de3d3d1033c88
Yago <yagojf27@gmail.com>
2022-09-12 20:30:44 -0300

    add função

--- projetobase/index.js
+++ projetobase/index.js
@@ -6,8 +6,13 @@ var Yago;
     Yago["GitHub"] = "https://github.com/yagosant";
     Yago["Linkdln"] = "https://www.linkedin.com/in/yago-dos-santos-ribeiro-a45b6990/?ppe=1";
     Yago["site"] = "https://yagosant.github.io";
+    Yago[Yago["idade"] = 28] = "idade";
 })(Yago || (Yago = {}));
 function getsite() { return Yago["site"]; }
 if (titleElement)
     titleElement.innerText = `Projeto Base do ${Yago["nome"]}`; // pegando os dados de um enum
 console.log(getsite());
+function soma(num1, num2) {
+    return console.log(num1 + num2);
+}
+soma(Yago.idade, 1);
--- projetobase/index.ts
+++ projetobase/index.ts
@@ -4,7 +4,8 @@ enum Yago {
     "nome"= "Yago dos Santos",
     "GitHub" = "https://github.com/yagosant",
     "Linkdln" = "https://www.linkedin.com/in/yago-dos-santos-ribeiro-a45b6990/?ppe=1",
-    "site" = "https://yagosant.github.io"
+    "site" = "https://yagosant.github.io",
+    "idade" = 28
     }
 
 function getsite() { return Yago["site"];}
@@ -12,4 +13,10 @@ function getsite() { return Yago["site"];}
 if (titleElement) titleElement.innerText = `Projeto Base do ${Yago["nome"]}`;// pegando os dados de um enum
 
 console.log(getsite());
+
+function soma(num1: number, num2: number) {
+    return console.log(num1 + num2);
+}
+
+soma(Yago.idade,1);
 
